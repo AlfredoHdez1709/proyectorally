1e417fdf1f656d67f8015f34c9d33dd25d08fb7c
Alfredo Hernandez Rodriguez <geekahr@MacBook-Pro-de-Alfredo.local>
2016-10-01 10:43:47 -0500

    listo pa heroku

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -122,5 +122,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+#Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
